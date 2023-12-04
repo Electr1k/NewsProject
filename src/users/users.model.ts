18c63c80bc4724d2a3cdc77dc4aaa72a6dc776cb
Electr1k <110545080+Electr1k@users.noreply.github.com>
2023-12-05 02:05:59 +0300

    env + users model

---
+++ src/users/users.model.ts
@@ -0,0 +1,19 @@
+import {Column, DataType, Model, Table} from "sequelize-typescript";
+
+interface UserCreationAttrs{
+    email: string;
+    password: string;
+}
+
+@Table({tableName: 'users'})
+export class User extends Model<User, UserCreationAttrs>{
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement:true, primaryKey: true})
+    id: number;
+
+    @Column({type: DataType.STRING, unique: true, allowNull: null})
+    email: string;
+
+    @Column({type: DataType.STRING, allowNull: null})
+    password: string;
+
+}
